0273ce9c5fc5c640a7e379c610dd4f042537e7b0
- <->
2018-01-08 10:18:59 +0100

    Initial commit

---
+++ Assets/Scripts/NewScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    public int entero;
+    public float dec;
+    public string frase;
+    public GameObject go;
+    public bool isTrue;
+    public Vector2 vector;
+
+
+    public void EscribirFrase()
+    {
+        Debug.Log("ASADASDASD");
+    }
+}
