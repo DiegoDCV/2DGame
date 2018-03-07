1293f67c1e1e0d0ba39dd083604d9332b5cc1d74
Diego <32306681+DiegoDCV@users.noreply.github.com>
2018-03-07 16:28:08 +0100

    Added some scripts

---
+++ Assets/Scripts/MyScripts/GamingManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamingManager : MonoBehaviour
+{
+    public PlayerMovement theplayer;
+    public Vector3 playerStartPoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerStartPoint = theplayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+
+    public IEnumerator RestartGameCo()
+    {
+        theplayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        theplayer.transform.position = playerStartPoint;
+        theplayer.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/MyScripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Rigidbody2D myRigidbody;
+    public BoxCollider2D myCollider;
+
+    public bool grounded;
+    public LayerMask WhatIsGround;
+
+    public Animator myAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody.GetComponent<Rigidbody2D>();
+
+        myCollider.GetComponent<BoxCollider2D>();
+
+        myAnimator.GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, WhatIsGround);
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+	}
+}
