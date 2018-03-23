946bd1b73beaaae27a327f33ffa51b1201cff7e3
Diego <32306681+DiegoDCV@users.noreply.github.com>
2018-03-23 03:39:34 +0100

    Finished All

--- Assets/Scripts/MyScripts/Menu/MenuButtons.cs
+++ Assets/Scripts/MyScripts/Menu/MenuButtons.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public float counter;
+    public bool loadingScene;
+
+    // Use this for initialization
+    void Start()
+    {
+        loadingScene = false;
+    }
+
+    void Update()
+    {
+        if(loadingScene == true)
+        {
+            counter -= Time.deltaTime;
+        }
+    }
 
     public void LoadScene(int buildIndex)
     {
+
         Time.timeScale = 1f;
+
         SceneManager.LoadScene(buildIndex);
+
     }
     public void LoadScene(string nameScene)
     {
         Time.timeScale = 1f;
+
         SceneManager.LoadScene(nameScene);
+
     }
     public void QuitGame()
     {
@@ -33,5 +55,10 @@ public class MenuButtons : MonoBehaviour
     {
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
+    }
+
+    public void BeginFade()
+    {
+        loadingScene = true;
     }
 }
